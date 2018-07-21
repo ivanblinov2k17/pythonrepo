9337e9d228d59e6b3702e20c0daaca2ea0ed5f4e
Ivan <ivanblinov2015@mail.ru>
2018-07-21 23:07:58 +0400

    added snake filling

--- untitled1/main.py
+++ untitled1/main.py
@@ -5,3 +5,7 @@ while temp != 'end':
     arr[line] = [int(i) for i in temp.split()]
     line += 1
     temp = input()
+arr1 = arr
+for i in range(len(arr[0])):
+    for j in range(len(arr)):
+        print(1)
