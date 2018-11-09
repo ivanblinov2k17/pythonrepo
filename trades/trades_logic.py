69bced198607311547bb7b7ada3ae7d998427903
ivanblinov2k17 <ivanblinov2015@mail.ru>
2018-11-09 19:58:55 +0300

    updated markets

--- trades/trades_logic.py
+++ trades/trades_logic.py
@@ -39,7 +39,25 @@ def longest_window(transactions):
             window_start = i
     for i in range(window_start, window_start + window_len):
         sum_transactions += transactions[i][1]*transactions[i][2]
-    return window_start, window_len, sum_transactions
+    starting_time = str(transactions[window_start][0].time())
+    return window_start, window_len, sum_transactions, starting_time
+
+
+def dividing_window_by_markets(transactions, market_list, window_start, window_len):
+    division = []
+    for market in market_list:
+        sum_transactions = 0
+        num_transactions = 0
+        start_transaction = 0
+        for i in range(window_start, window_start+window_len):
+            if transactions[i][3] == market:
+                if num_transactions == 0:
+                    start_transaction = i
+                num_transactions += 1
+                sum_transactions += transactions[i][1] * transactions[i][2]
+        trans_time = str(transactions[start_transaction][0].time())
+        division.append([market, trans_time, num_transactions, sum_transactions])
+    return division
 
 
 def dividing_transactions_by_market(transactions, market):
@@ -57,25 +75,9 @@ def longest_window_by_market(transactions, market):
     return longest_window(transactions_by_market)
 
 
-def dividing_window_by_markets(transactions, market_list, window_start, window_len):
-    division = []
-    for market in market_list:
-        sum_transactions = 0
-        num_transactions = 0
-        start_transaction = 0
-        for i in range(window_start, window_start+window_len):
-            if transactions[i][3] == market:
-                if num_transactions == 0:
-                    start_transaction = i
-                num_transactions += 1
-                sum_transactions += transactions[i][1] * transactions[i][2]
-        division.append([market, start_transaction, num_transactions, sum_transactions])
-    return division
-
-
 def longest_windows_by_markets(transactions, market_list):
     windows = []
     for market in market_list:
         window = longest_window_by_market(transactions, market)
-        windows.append([market, window])
+        windows.append([market, window[3], window[1], window[2]])
     return windows
--- trades/trades_main.py
+++ trades/trades_main.py
@@ -1,11 +1,17 @@
 import trades.trades_logic as logic
 
 transactions, market_list = logic.initArr('trades.csv')
-main_window_start, main_window_len, main_window_sum = logic.longest_window(transactions)
-division = logic.dividing_window_by_markets(transactions, market_list, main_window_start, main_window_len)
+
+main_window_start, main_window_len, main_window_sum, main_window_start_time\
+    = logic.longest_window(transactions)
+
+division = logic.dividing_window_by_markets(transactions, market_list,
+                                            main_window_start, main_window_len)
+
 windows_by_markets = logic.longest_windows_by_markets(transactions, market_list)
+
 print("Main window")
-print(main_window_start, main_window_len, main_window_sum)
+print(main_window_start_time, main_window_len, main_window_sum)
 print("Dividing main window by markets")
 for div in division:
     print(div)
